4047b8fafe2e253666d71204f45711a503946d0f
murali26132 <70324057+murali26132@users.noreply.github.com>
2020-08-28 16:16:41 +0530

    userdetails changed

--- src/app/usersdetails/usersdetails.component.ts
+++ src/app/usersdetails/usersdetails.component.ts
@@ -6,54 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usersdetails.component.css']
 })
 export class UsersdetailsComponent implements OnInit {
-  users =  [
-    {
-        'name': 'Xyz',
-        'subscriptionId':'123',
-        'subscriptionStartDate':'123',
-        'subscriptionEndDate':'123',
-        'phoneNo':'1234567890'
-    },
-    {
-        'name': 'Abc',
-        'subscriptionId':'456',
-        'subscriptionStartDate':'456',
-        'subscriptionEndDate':'456',
-        'phoneNo':'9999999999'
-  },
-  {
-        'name': 'Xyz',
-        'subscriptionId':'123',
-        'subscriptionStartDate':'123',
-        'subscriptionEndDate':'123',
-        'phoneNo':'1234567890'
-  },
-    {
-        'name': 'Abc',
-        'subscriptionId':'456',
-        'subscriptionStartDate':'456',
-        'subscriptionEndDate':'456',
-        'phoneNo':'9999999999'
-    },
-    {
-      'name': 'Xyz',
-        'subscriptionId':'123',
-        'subscriptionStartDate':'123',
-        'subscriptionEndDate':'123',
-        'phoneNo':'1234567890'
-    },
-    {
-      'name': 'Abc',
-        'subscriptionId':'456',
-        'subscriptionStartDate':'456',
-        'subscriptionEndDate':'456',
-        'phoneNo':'9999999999'
-    },
+  
+  firstname:string;
+  users=[];
+  usersList=[
+      
+    { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'9-10-20',phoneNo:54545456454 },
+    { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'10-10-0',phoneNo:54545456454 },
+    { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'10-10-0',phoneNo:54545456454 },
+    { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'12-10-0',phoneNo:54545456454 },
+    { Name:'sita', subscriptionId:'1', subscriptionStartDate:'4-1-2019', subscriptionEndDate:'13-10-0',phoneNo:54545456454 },
+    
   ]
 
   constructor() { }
+  
 
   ngOnInit(): void {
+    this.users = this.usersList;
+    // this.users=[
+      
+    //   { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'23-10-0',phoneNo:54545456454 },
+    //   { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'23-10-0',phoneNo:54545456454 },
+    //   { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'23-10-0',phoneNo:54545456454 },
+    //   { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'23-10-0',phoneNo:54545456454 },
+    //   { Name:'murali', subscriptionId:'1', subscriptionStartDate:'22-9-0', subscriptionEndDate:'23-10-0',phoneNo:54545456454 },
+      
+    // ]
+
+  }
+  search(){
+    debugger
+  
+     this.users = this.usersList.filter(res => 
+       res.subscriptionEndDate.toLocaleLowerCase().indexOf(this.firstname.toLocaleLowerCase()) > -1 
+     )
+    }
   }
 
-}
+
