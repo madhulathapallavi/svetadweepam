9cc46f2b815fd7d8d519db2fdc52dbd57d1cf1d2
murali26132 <70324057+murali26132@users.noreply.github.com>
2020-08-29 13:06:59 +0530

    dashboard changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  year = 2016
+  yearlist = [];
+  //   {year:"2020",
+  //     month:[
+  //       { month:["jan","feb","march","april","may","jun","july","aug","sep","oct","nov","dec"]}
+  //     ]
+  //   },
+  //   {year:"2019",
+  //     month:[
+  //       { month:["jan","feb","march","april","may","jun","july","aug","sep","oct","nov","dec"]}
+  //     ]
+  //   },
+  //   {year:"2018",
+  //     month:[
+  //       { month:["jan","feb","march","april","may","jun","july","aug","sep","oct","nov","dec"]}
+  //     ]
+  //   },
+
+  // ]
+  months = [
+          { month:["jan","feb","march","april","may","jun","july","aug","sep","oct","nov","dec"]}
+        ]
 
   constructor() { }
 
   ngOnInit(): void {
+
+    var currentdate = Number(((new Date()).getFullYear()));
+
+    for (let index = this.year; index <= currentdate; index++) {
+      this.yearlist.push(
+        {
+          year: index,
+          month: this.months
+        }
+      )
+
+    }
+    console.log(this.yearlist)
+
   }
+
 
 }
