09b8b89aa4e5eab5e788fd7e4589f715a86fbf7d
Your Name <you@example.com>
2020-10-07 14:03:30 +0530

    userform.ts changes

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -3,8 +3,7 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ConfirmedValidator } from './confirmed.validator';
-
+import { AuthService } from '../shared/auth/auth.service';
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
@@ -14,45 +13,78 @@ export class UserformComponent implements OnInit {
   newuserform:FormGroup;
   // submitted = false;
 
-  firstname:any;
-  lastname:string;
-  email:any;
-  phonenumber:number;
-  password:any;
-  confirmpassword:any;
+  firstnameBind:any;
+  lastnameBind:any;
+  emailBind:any;
+  phonenumberBind:any;
+  passwordBind:any;
+  cpasswordBind:any;
+  userid:string = '';
 
-  constructor(private router:Router,private formBuilder: FormBuilder,private toastr: ToastrService) { 
-    
-  }
+  firstnameIsEmpty = false;
+  lastnameIsEmpty = false;
+  emailIsEmpty = false;
+  phonenumberIsEmpty = false;
+  passwordIsEmpty = false;
+  cpasswordIsEmpty = false;
+  showerror = false;
+
+
+  constructor(private router:Router,private formBuilder: FormBuilder,private toaster: ToastrService,private auth:AuthService) { 
+   
+    // auth.getUserDetails().then(value =>{
+    //   console.log(value);
+    //   this.firstnameBind = value[0].user_firstname;
+    //   this.lastnameBind = value[0].user_lastname;
+    //   this.emailBind = value[0].user_email;
+    //   this.passwordBind = value[0].user_password;
+    //   this.phonenumberBind = value[0].user_mobile_number;
+    // });
+   }
   clearForm(){
-      this.firstname='';
-      this.lastname='';
-      this.email='';
-      this.phonenumber;
-      this.password='';
-      this.confirmpassword='';
+      this.firstnameBind='';
+      this.lastnameBind='';
+      this.emailBind='';
+      this.phonenumberBind;
+      this.passwordBind='';
+      this.cpasswordBind='';
     }
 
-  ngOnInit(): void {
-    this.newuserform = this.formBuilder.group({
-      firstname:['',Validators.required],
-      lastname:['',Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phonenumber:['',[Validators.required, Validators.maxLength(10)]],
-      // createpassword: ['', [Validators.required, Validators.minLength(6)]],
-      // confirmpassword:['',[Validators.required,  Validators.minLength(6)]]
-      password: ['', [Validators.required]],
-      confirmpassword: ['', [Validators.required]]
-    }, { 
-      validator: ConfirmedValidator('password', 'confirmpassword')
-    });
-  }
+  ngOnInit(){
+  ; 
+}
   
-  get formData() { return this.newuserform.controls; }
+onSubmit(f:NgForm){
+   
+      
+  // this.auth.newUserDetails({
+  //       userid:this.userid,
+  //       firstName:f.value.firstname,
+  //       lastName : f.value.lastname,
+  //       emailId : f.value.email,
+  //       phoneNumber: f.value.phonenumber,
+  //       password:f.value.password,
+  //     }).then(value =>{
+  //       console.log(value);
+  //       this.toaster.success('NewUser Details Received');
+  //       this.router.navigate(['/Usersdetails']);
+  //     }).catch(err=>{
+  //       console.log(err);
+  //       this.toaster.error("Details Not Received");
+  //     })
+      
+    
+    console.log(f.value);
+  // 
+}
+
   
-  onSubmit(){
-    this.toastr.success('NewUser Details Received');
-    console.log(this.newuserform.value);
-  }
+    verifyPassword(f:NgForm){
+        if(f.value.passwordBind == f.value.cpasswordBind){
+            this.showerror = false;
+        }else{
+            this.showerror = true;
+        }
+    }
 
 }
